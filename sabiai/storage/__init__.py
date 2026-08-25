305df5be3e288d543b93c9484a82189b09c2634e
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 13:12:55 +0100

    Export bookmaker offer observation store

--- sabiai/storage/__init__.py
+++ sabiai/storage/__init__.py
@@ -3,6 +3,7 @@ from .bankroll import BankrollLedger, LedgerEntry
 from .dashboard_reads import DashboardReadService
 from .drafts import TicketDraft, TicketDraftStore
 from .history import HistoryService
+from .offers import OfferObservation, OfferObservationStore
 from .sqlite import SabiDatabase
 
 __all__ = [
@@ -10,6 +11,8 @@ __all__ = [
     "DashboardReadService",
     "HistoryService",
     "LedgerEntry",
+    "OfferObservation",
+    "OfferObservationStore",
     "PerformanceAnalytics",
     "SabiDatabase",
     "TicketDraft",
