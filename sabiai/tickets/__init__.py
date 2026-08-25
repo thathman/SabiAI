c35ff16c6642e43513bc5b4eba14ee93639ce6e0
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 08:41:16 +0100

    feat(v2): export ticket variant planning

--- sabiai/tickets/__init__.py
+++ sabiai/tickets/__init__.py
@@ -1,14 +1,18 @@
 from .normalize import TicketIssue, TicketNormalization, TicketNormalizer
 from .text_import import ExtractedTextLeg, TextTicketExtraction, TicketTextImporter
+from .variants import MarketChangeSuggestion, RankedLeg, TicketVariantPlanner
 from .workshop import TicketWorkshop, TicketWorkshopError
 
 __all__ = [
     "ExtractedTextLeg",
+    "MarketChangeSuggestion",
+    "RankedLeg",
     "TextTicketExtraction",
     "TicketIssue",
     "TicketNormalization",
     "TicketNormalizer",
     "TicketTextImporter",
+    "TicketVariantPlanner",
     "TicketWorkshop",
     "TicketWorkshopError",
 ]
