5a7adfd2d6bd961590f0f513ea83b79ea3ed9124
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 01:07:45 +0100

    feat(v2): expose bookmaker adapter registry

--- sabiai/bookmakers/__init__.py
+++ sabiai/bookmakers/__init__.py
@@ -1,3 +1,18 @@
+from .adapters import (
+    AdapterRegistry,
+    AdapterStatus,
+    BookmakerCapability,
+    CommandBookmakerAdapter,
+    legacy_command_adapters,
+)
 from .registry import BookmakerRegistry, default_bookmakers
 
-__all__ = ["BookmakerRegistry", "default_bookmakers"]
+__all__ = [
+    "AdapterRegistry",
+    "AdapterStatus",
+    "BookmakerCapability",
+    "BookmakerRegistry",
+    "CommandBookmakerAdapter",
+    "default_bookmakers",
+    "legacy_command_adapters",
+]
