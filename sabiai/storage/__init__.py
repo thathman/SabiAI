e40b3bbecaa17a6f2581f971533d10d69f3e23ec
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 08:44:56 +0100

    feat(v2): export detailed dashboard read service

--- sabiai/storage/__init__.py
+++ sabiai/storage/__init__.py
@@ -1,11 +1,13 @@
 from .analytics import PerformanceAnalytics
 from .bankroll import BankrollLedger, LedgerEntry
+from .dashboard_reads import DashboardReadService
 from .drafts import TicketDraft, TicketDraftStore
 from .history import HistoryService
 from .sqlite import SabiDatabase
 
 __all__ = [
     "BankrollLedger",
+    "DashboardReadService",
     "HistoryService",
     "LedgerEntry",
     "PerformanceAnalytics",
