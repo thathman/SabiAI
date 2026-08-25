203f4533fa2536a4d787edbba4f86d0a243406bc
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 08:37:34 +0100

    feat(v2): export default source adapters and catalog

--- sabiai/sources/__init__.py
+++ sabiai/sources/__init__.py
@@ -1,3 +1,5 @@
+from .catalog import SourceBundle, default_source_bundle
+from .football_data import FootballDataAdapter
 from .health import SourceHealth, SourceHealthService
 from .registry import (
     AccessDecision,
@@ -7,10 +9,13 @@ from .registry import (
     SourceRegistry,
 )
 from .service import SourceRequest, SourceResponse, SourceService
+from .thesportsdb import TheSportsDBAdapter
 
 __all__ = [
     "AccessDecision",
+    "FootballDataAdapter",
     "Source",
+    "SourceBundle",
     "SourceCost",
     "SourceHealth",
     "SourceHealthService",
@@ -19,4 +24,6 @@ __all__ = [
     "SourceRequest",
     "SourceResponse",
     "SourceService",
+    "TheSportsDBAdapter",
+    "default_source_bundle",
 ]
