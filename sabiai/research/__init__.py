272dc3b1e26521b5a02296d8b3e366d14bd65d92
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 11:08:45 +0100

    feat(v2): export evidence packet intake

--- sabiai/research/__init__.py
+++ sabiai/research/__init__.py
@@ -1,5 +1,6 @@
 from .case import EvidenceConflict, ResearchAssessment, ResearchCaseService
 from .evidence import Evidence, EvidenceStore
+from .intake import EvidencePacketItem, EvidencePacketResult, EvidencePacketService
 from .orchestrator import ResearchFinding, ResearchOrchestrator, ResearchRun, ResearchRunItem
 from .reviewer import ReviewQuestion, SkepticReviewPlan, SkepticReviewPlanner
 from .synthesis import ResearchSummary, ResearchSynthesizer
@@ -8,6 +9,9 @@ from .tasks import ResearchTask, ResearchTaskPlanner
 __all__ = [
     "Evidence",
     "EvidenceConflict",
+    "EvidencePacketItem",
+    "EvidencePacketResult",
+    "EvidencePacketService",
     "EvidenceStore",
     "ResearchAssessment",
     "ResearchCaseService",
