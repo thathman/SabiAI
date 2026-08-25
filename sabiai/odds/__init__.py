46dfb336ee3c16e11d2c53d6ee4216178c7b5846
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 13:20:52 +0100

    Export price comparison service

--- sabiai/odds/__init__.py
+++ sabiai/odds/__init__.py
@@ -1,3 +1,19 @@
 from .arbitrage import ArbitrageEngine, ArbitrageResult, PriceQuote, SettlementRules, StakeAllocation
+from .comparison import (
+    ComparedPrice,
+    PriceComparisonResult,
+    PriceComparisonService,
+    SelectionPriceComparison,
+)
 
-__all__ = ["ArbitrageEngine", "ArbitrageResult", "PriceQuote", "SettlementRules", "StakeAllocation"]
+__all__ = [
+    "ArbitrageEngine",
+    "ArbitrageResult",
+    "ComparedPrice",
+    "PriceComparisonResult",
+    "PriceComparisonService",
+    "PriceQuote",
+    "SelectionPriceComparison",
+    "SettlementRules",
+    "StakeAllocation",
+]
