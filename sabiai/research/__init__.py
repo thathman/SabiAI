94ad482d5a2990bffae8bfe17090d8bc77139fcc
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 14:36:55 +0100

    Export persistent research case store

--- sabiai/research/__init__.py
+++ sabiai/research/__init__.py
@@ -2,6 +2,7 @@ from .case import EvidenceConflict, ResearchAssessment, ResearchCaseService
 from .evidence import Evidence, EvidenceStore
 from .intake import EvidencePacketItem, EvidencePacketResult, EvidencePacketService
 from .orchestrator import ResearchFinding, ResearchOrchestrator, ResearchRun, ResearchRunItem
+from .persistence import PersistentResearchCase, ResearchCaseStore
 from .reviewer import ReviewQuestion, SkepticReviewPlan, SkepticReviewPlanner
 from .synthesis import ResearchSummary, ResearchSynthesizer
 from .tasks import ResearchTask, ResearchTaskPlanner
@@ -13,8 +14,10 @@ __all__ = [
     "EvidencePacketResult",
     "EvidencePacketService",
     "EvidenceStore",
+    "PersistentResearchCase",
     "ResearchAssessment",
     "ResearchCaseService",
+    "ResearchCaseStore",
     "ResearchFinding",
     "ResearchOrchestrator",
     "ResearchRun",
