ee6741e917f7df6184c02b7c01971b05f22ab9e2
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-25 01:12:48 +0100

    feat(v2): expose copied-text ticket importer

--- sabiai/tickets/__init__.py
+++ sabiai/tickets/__init__.py
@@ -1,10 +1,14 @@
 from .normalize import TicketIssue, TicketNormalization, TicketNormalizer
+from .text_import import ExtractedTextLeg, TextTicketExtraction, TicketTextImporter
 from .workshop import TicketWorkshop, TicketWorkshopError
 
 __all__ = [
+    "ExtractedTextLeg",
+    "TextTicketExtraction",
     "TicketIssue",
     "TicketNormalization",
     "TicketNormalizer",
+    "TicketTextImporter",
     "TicketWorkshop",
     "TicketWorkshopError",
 ]
